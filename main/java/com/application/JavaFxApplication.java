a37bf745eb27d801d48685354aa44f55da59c497
JarlBurget <jarl-ivor.burget@voco.ee>
2025-10-21 13:48:35 +0300

    2 nuppu ja pealkiri(title)

--- main/java/com/application/JavaFxApplication.java
+++ main/java/com/application/JavaFxApplication.java
@@ -1,13 +1,29 @@
 package com.application;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Label; // ✅ Import the Label class
+
 import javafx.stage.Stage;
 
 public class JavaFxApplication extends Application {
 
     @Override
     public void start(Stage window) {
-        window.setTitle("Max banger application!");
+        Label textComponent = new Label("Text element");
+
+        Button button1 = new Button("First Button");
+        Button button2 = new Button("Second Button");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().addAll(button1, button2);
+
+        Scene scene = new Scene(componentGroup, 300, 100);
+
+        window.setTitle("Koos pealkirjaga");
+        window.setScene(scene);
         window.show();
     }
 
